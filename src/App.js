0941bf4b59882e465aa385bdc7d0f3a063cc4387
Shelton Bilgates <sheltonbilgates@gmail.com>
2023-11-27 20:00:59 +0530

    ch

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
 
   return (
     <div className="App">
-      <div className="container">s
+      <div className="container">
         <h2>BMI Calculator</h2>
         <br />
         <form onSubmit={calcBmi}>
